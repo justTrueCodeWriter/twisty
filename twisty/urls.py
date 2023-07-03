41b6c5dd4ede5e7f612075730d7c1a1e0cebfcab
jojopko <gamerat.github@gmail.com>
2023-07-03 19:12:37 +0300

    add login

--- twisty/urls.py
+++ twisty/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from web.views import IndexPage
 
 urlpatterns = [
     path('', IndexPage.as_view(), name="index"),
     path('admin/', admin.site.urls, name="admin"),
+    path("change-password/", auth_views.PasswordChangeView.as_view()),
+    path("login/", auth_views.LoginView.as_view(), name="login")
 ]
 
 
