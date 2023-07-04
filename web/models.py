ff82823c5a8f2009710401b8f0aebc64702a18cb
jojopko <gamerat.github@gmail.com>
2023-07-04 19:00:15 +0300

    upd models

--- web/models.py
+++ web/models.py
@@ -3,11 +3,22 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.OneToOneField(User,
+                                 on_delete=models.CASCADE,
+                                 primary_key=True)
     name = models.CharField(max_length=32, default="Linus")
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
-    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    author = models.ForeignKey(Profile,
+                               on_delete=models.PROTECT)
     text = models.CharField(max_length=281)  # Twitter - suck, with 280 characters
+
+    def __str__(self):
+        return self.text[:20]
+
+
 
