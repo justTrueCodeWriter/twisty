1d9f48610becfd9adf2d3b71c90adc612d7ea924
jojopko <gamerat.github@gmail.com>
2023-07-04 19:10:24 +0300

    add login system

--- twisty/urls.py
+++ twisty/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-from web.views import IndexPage
+from web.views import IndexPage, CreatePost
 
 urlpatterns = [
     path('', IndexPage.as_view(), name="index"),
+    path('create/', CreatePost.as_view(), name="create_post"),
     path('admin/', admin.site.urls, name="admin"),
-    path("change-password/", auth_views.PasswordChangeView.as_view()),
-    path("login/", auth_views.LoginView.as_view(), name="login")
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout")
 ]
 
 
--- web/views.py
+++ web/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views.generic.base import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from web.models import Post, Profile
 
 
 class CreatePost(View):
     def get(self, request, *args, **kwargs):
-        ...
+        return render(request, "web/create.html")
 
     def post(self, request, *args, **kwargs):
-        ...
+        user_profile = Profile.objects.get(owner=request.user)
+        if user_profile:
+            new_post = Post(text=request.POST["text"], author=user_profile)
+            new_post.save()
+        return HttpResponseRedirect(reverse("index"))
 
 
 class IndexPage(View):
