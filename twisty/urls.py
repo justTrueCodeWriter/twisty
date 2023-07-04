e46bb12fb983c32a16154552e99c3b2ae5f38f25
justTrueCodeWriter <>
2023-07-05 00:18:19 +0300

    add user profile

--- twisty/urls.py
+++ twisty/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-from web.views import IndexPage, CreatePost
+from web.views import IndexPage, CreatePost, ProfilePage, EditProfile
 
 urlpatterns = [
     path('', IndexPage.as_view(), name="index"),
+    path('profile/', ProfilePage.as_view(), name="profile"),
+    path('editprofile/', EditProfile.as_view(), name="edit_profile"), 
     path('create/', CreatePost.as_view(), name="create_post"),
     path('admin/', admin.site.urls, name="admin"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
