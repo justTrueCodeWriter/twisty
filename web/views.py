50f720ea65bc6a4327d804dd9b3156adac30e444
jojopko <gamerat.github@gmail.com>
2023-07-04 12:20:46 +0300

    add html for index page

--- web/views.py
+++ web/views.py
@@ -3,6 +3,14 @@ from django.views.generic.base import View
 from django.http import HttpResponse
 
 
+class CreatePost(View):
+    def get(self, request, *args, **kwargs):
+        ...
+
+    def post(self, request, *args, **kwargs):
+        ...
+
+
 class IndexPage(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello, World!")
+        return render(request, "web/index.html")
